9d165d64606ac2344c3dee1f33731f9fe4fd68f6
zicog <zicog@example.com>
2020-01-15 21:14:15 +0200

    Added cpp version using new and delete[] for arrays

---
+++ loop_blocking.cpp
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <time.h>
+#include <inttypes.h>
+#include <stdlib.h>
+
+#define MAX 8192
+#define BLOCK_SIZE 64
+
+void do_it_0 (int32_t a[MAX][MAX], int32_t b[MAX][MAX]) {
+    for (size_t i = 0; i < MAX;     i++) {
+        for (size_t j = 0; j < MAX; j++) {
+            a[i][j] += b[j][i];
+        }
+    }
+}
+
+void do_it_1 (int32_t a[MAX][MAX], int32_t b[][MAX]) {
+    for (size_t i = 0; i < MAX; i += BLOCK_SIZE) {
+        for (size_t j = 0; j < MAX; j += BLOCK_SIZE) {
+            for (size_t ii = i; ii < i + BLOCK_SIZE; ii++) {
+                for (size_t jj = j; jj < j + BLOCK_SIZE; jj++) {
+                    a[ii][jj] += b[jj][ii];
+                }
+             }
+        }
+    }
+}
+
+void fill_arrays (int32_t a[MAX][MAX], int32_t b[MAX][MAX]) {
+    int32_t count = 0.0; 
+    for (size_t i = 0; i < MAX; i++) {
+        for (size_t j =0; j < MAX; j++) {
+            a[i][j] = count;
+            b[j][i] = -count;
+            count += 1.0;
+        }
+    }
+}
+
+void print_array (int32_t a[MAX][MAX]) {
+    for (size_t i = 0; i < 32; i++) {
+        for (size_t j = 0; j < 32; j++) {
+            printf("%d, ", a[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// Return a timestamp in nano second resolution.
+uint64_t time_ns( void ) {
+  struct timespec now;
+  clock_gettime( CLOCK_MONOTONIC, &now );
+  return (uint64_t)now.tv_sec * UINT64_C(1000000000) + (uint64_t)now.tv_nsec;
+}
+
+int main () {
+    printf("MAX:        %d\n", MAX);
+    printf("BLOCK_SIZE: %d\n", BLOCK_SIZE);
+
+    auto a = static_cast<int32_t (*)[MAX]>(malloc(sizeof(int32_t[MAX][MAX]))); 
+    auto b = static_cast<int32_t (*)[MAX]>(malloc(sizeof(int32_t[MAX][MAX]))); 
+    {
+        fill_arrays(a, b);
+        uint64_t startTime = time_ns();
+        do_it_0(a, b);
+        uint64_t endTime = time_ns();
+        printf ("do_it_0:    %" PRIu64 "ms\n", (endTime - startTime) / 1000000U);
+        //print_array(a);
+    }
+    {
+        fill_arrays(a, b);
+        uint64_t startTime = time_ns();
+        do_it_1(a, b);
+        uint64_t endTime = time_ns();
+        printf ("do_it_1:    %" PRIu64 "ms\n", (endTime - startTime) / 1000000U);
+        //print_array(a);
+    }
+    delete[] a;
+    delete[] b;
+}
+

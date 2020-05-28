bc3be4e7628ca6b18984ee336dedbbc145224916
zicog <you@example.com>
2020-05-28 14:12:32 +0300

    Added 64 bit notes to README

--- loop_blocking_64.c
+++ loop_blocking_64.c
@@ -40,7 +40,7 @@ void fill_arrays (int64_t a[MAX][MAX], int64_t b[MAX][MAX]) {
 void print_array (int64_t a[MAX][MAX]) {
     for (size_t i = 0; i < 32; i++) {
         for (size_t j = 0; j < 32; j++) {
-            printf("%ld, ", a[i][j]);
+            printf("%lld, ", a[i][j]);
         }
         printf("\n");
     }
